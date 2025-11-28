4af97f0efae9e10b315e71dfd1dbc56d6f0bbf8b
Abhiram-JV <jalduvabhiram@gmail.com>
2025-11-28 15:02:12 +0530

    Fix GitHub Pages configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   trailingSlash: true,
+  basePath: '/2400032963-FEDF-End_Sem',
+  assetPrefix: '/2400032963-FEDF-End_Sem/',
   images: {
     unoptimized: true
   }
