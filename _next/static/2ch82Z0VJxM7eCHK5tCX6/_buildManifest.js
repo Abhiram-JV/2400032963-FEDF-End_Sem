50cdf06c2bf6569959a973337adb2ee438e4fcc6
Abhiram-JV <jalduvabhiram@gmail.com>
2025-11-28 15:12:25 +0530

    Updates

---
+++ _next/static/2ch82Z0VJxM7eCHK5tCX6/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/921-0bdbbe9252522e82.js","static/chunks/pages/index-90eb09a536509844.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
